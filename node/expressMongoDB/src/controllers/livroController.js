d1af6793249d5a4875b2e1927741c48313919e80
Julia Ferreira <ferreirajulia325@gmail.com>
2026-01-27 20:02:46 -0300

    alguns ajustes

--- node/expressMongoDB/src/controllers/livroController.js
+++ node/expressMongoDB/src/controllers/livroController.js
@@ -1,4 +1,5 @@
 import livro from "../models/Livro.js";
+import { autor } from "../models/Autor.js";
 
 class LivroController {
 
@@ -23,20 +24,27 @@ class LivroController {
     };
 
     static async cadastrarLivros(req, res) {
+        const novoLivro = req.body;
+
         try {
-            const novoLivro = await livro.create(req.body);
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc } };
+            await livro.create(livroCompleto);
             res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
         } catch (error) {
             res.status(500).json({ message: `${error.message} - falha ao cadastrar livro` });
         }
     }
 
-    
+
     static async atualizarLivro(req, res) {
+        const novoLivro = req.body;
         try {
             const id = req.params.id;
-            await livro.findByIdAndUpdate(id, req.body); // findByIdAndUpdate() método do mongoose
-            res.status(200).json({message: "livro atualizado"});
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc } };
+            await livro.findByIdAndUpdate(id, livroCompleto); // findByIdAndUpdate() método do mongoose
+            res.status(200).json({ message: "livro atualizado" });
         } catch (error) {
             res.status(500).json({ message: `${error.message} - falha na requisição` });
         }
@@ -46,11 +54,23 @@ class LivroController {
         try {
             const id = req.params.id;
             await livro.findByIdAndDelete(id); // findByIdAndDelete() método do mongoose
-            res.status(200).json({message: "livro excluído com sucesso"});
+            res.status(200).json({ message: "livro excluído com sucesso" });
         } catch (error) {
             res.status(500).json({ message: `${error.message} - falha na exclusão` });
         }
     };
+
+    static async listarLivrosPorEditora(req, res) {
+        const editora = req.query.editora;
+
+        try {
+            const livrosPorEditora = await livro.find({ editora: editora });
+            res.status(200).json(livrosPorEditora);
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha na busca` });
+        }
+
+    }
 
 };
 
--- node/expressMongoDB/src/routes/index.js
+++ node/expressMongoDB/src/routes/index.js
@@ -5,8 +5,7 @@ import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
     app.route('/').get((req, res) => req.status(200).send("Curso de Node.js"));
-    app.use(express.json(), livros); // inclui midlewares na instancia do express
-    app.use(express.json(), autores); // inclui midlewares na instancia do express
+    app.use(express.json(), livros, autores); // inclui midlewares na instancia do express
 };
 
 export default routes;
