6a26872fb0e97f40986bbc0292d1bc65ccb48732
Julia Ferreira <ferreirajulia325@gmail.com>
2026-02-25 19:51:58 -0300

    alura-books

---
+++ node/alura-books/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Logo from './componentes/Logo';
+import OpcoesHeader from './componentes/OpcoesHeader';
+import Icones from './componentes/Icones';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Logo></Logo>
+        <OpcoesHeader></OpcoesHeader>
+        <Icones></Icones>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
